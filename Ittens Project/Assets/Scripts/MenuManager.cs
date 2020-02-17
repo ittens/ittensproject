cec396bc6d31337fef696d3f1a9ba2e7e8f0fd58
daveekh <dav5vid@gmail.com>
2020-02-17 02:23:31 +0100

    Menu & Tilemaps

---
+++ Ittens Project/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// MenuManager to skrypt to zarządzania buttonami i opcjami w menu głównym
+
+
+public class MenuManager : MonoBehaviour
+{
+
+        public void NewGame() {
+            SceneManager.LoadScene("New Game");
+
+        }
+
+        public void LoadGame() {
+
+
+        }
+
+        public void Options() {
+
+
+        }
+
+        public void Credits() {
+
+
+        }
+
+        public void Exit() {
+            Application.Quit();
+
+        }
+
+
+
+}
