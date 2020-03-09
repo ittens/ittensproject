1b5d86b64a3075fd03eda1b63bb7a8406c8458ad
daveekh <dav5vid@gmail.com>
2020-03-09 21:52:49 +0100

    Player Movement

---
+++ Ittens Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Vector2 playerInput;   
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sr;
+
+    private float moveSpeed = 6.5f;
+
+    void FixedUpdate()
+    {
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = playerInput.normalized * moveSpeed;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) sr.flipX = true;
+        if(Input.GetKeyDown(KeyCode.D)) sr.flipX = false;
+    }
+}
