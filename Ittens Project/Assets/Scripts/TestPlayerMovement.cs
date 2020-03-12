d8c3559164c216a6c093dfe32bd777cf23f0849d
daveekh <dav5vid@gmail.com>
2020-03-12 23:13:40 +0100

    Slime v1
    Slime Monster Animation Idle + Moving

---
+++ Ittens Project/Assets/Scripts/TestPlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour
+{
+    private Vector2 playerInput;   
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
+
+    private float moveSpeed = 3f;
+
+    void FixedUpdate()
+    {
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = playerInput.normalized * moveSpeed;
+    }
+
+
+    void Update()
+    {
+        anim.SetFloat("moveSpeedX", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("moveSpeedY", Mathf.Abs(rb.velocity.y));
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetBool("isWalkingUp", true);
+            anim.SetBool("isWalkingDown", false);
+            anim.SetBool("isWalkingLeft", false);
+            anim.SetBool("isWalkingRight", false);
+        }       
+
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetBool("isWalkingUp", false);
+            anim.SetBool("isWalkingDown", true);
+            anim.SetBool("isWalkingLeft", false);
+            anim.SetBool("isWalkingRight", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool("isWalkingUp", false);
+            anim.SetBool("isWalkingDown", false);
+            anim.SetBool("isWalkingLeft", true);
+            anim.SetBool("isWalkingRight", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("isWalkingUp", false);
+            anim.SetBool("isWalkingDown", false);
+            anim.SetBool("isWalkingLeft", false);
+            anim.SetBool("isWalkingRight", true);
+        }
+    }
+}
---
+++ Ittens Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    private int number;
+
+    void Start()
+    {
+        for (int i=0; i<100; i++) {
+            number = Random.Range(1, 5);
+            Debug.Log(number);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
