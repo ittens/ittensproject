49805fd006308bf2df290f97e8455b23e51bb7e1
Kate <mitruskasia@gmail.com>
2020-04-04 13:35:00 +0200

    Drobne zmianki

--- Ittens Project/Assets/Scripts/MenuScripts/MenuManager.cs
+++ Ittens Project/Assets/Scripts/MenuScripts/MenuManager.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// MenuManager to skrypt to zarządzania buttonami i opcjami w menu głównym
+/// Skrypt do zarządzania buttonami i opcjami w menu głównym
 
 
 public class MenuManager : MonoBehaviour
 {
 
-        public void NewGame() {
+        public void NewGame() 
+        {
             SceneManager.LoadScene("New Game");
+        }
+
+        public void LoadGame() 
+        {
 
         }
 
-        public void LoadGame() {
-            // Testowy komentarz
-
+        public void Options() 
+        {
+            
         }
 
-        public void Options() {
-
-
+        public void Credits() 
+        {
+            
         }
 
-        public void Credits() {
-
-
-        }
-
-        public void Exit() {
+        public void Exit() 
+        {
             Application.Quit();
-
         }
 
 
